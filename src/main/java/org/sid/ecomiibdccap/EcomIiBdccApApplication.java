2a9bd7bb498c64baccdfee9039c85eaa31a04a63
Nessrine004 <Nessrine.Bekri@emsi-edu.ma>
2025-11-09 18:13:02 +0100

    Partie_1

---
+++ src/main/java/org/sid/ecomiibdccap/EcomIiBdccApApplication.java
@@ -0,0 +1,13 @@
+package org.sid.ecomiibdccap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomIiBdccApApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomIiBdccApApplication.class, args);
+    }
+
+}
