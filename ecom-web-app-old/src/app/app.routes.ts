46f5142accf71e05598d999b938b6e2b407d92dc
Nessrine004 <nessrine.bekri@emsi-edu.ma>
2025-12-12 11:58:21 +0100

    TEST

--- ecom-web-app-old/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { Products } from './products/products';
-
-export const routes: Routes = [
-  {
-    path: 'products',
-    component: Products
-  }
-];
